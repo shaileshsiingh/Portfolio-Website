c32baa27c6ea0ee31f09c64044b51aa17a344cb4
Shailesh Singh <shaileshsiingh@gmail.com>
2024-02-21 14:46:31 +0530

    AcheivementsSection added

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSections";
 import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <AboutSection />
         <ProjectsSection/>
         <EmailSection/>
+        <AchievementsSection/>
       </div>
       <Footer/>
     </main>
